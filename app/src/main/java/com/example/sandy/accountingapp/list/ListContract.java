e7c90860096059a488f5b1a13cc0dda4d3fa5ba5
sandy <crazysandy7342@gmail.com>
2019-07-01 08:51:21 +0800

    version2 zxx

---
+++ app/src/main/java/com/example/sandy/accountingapp/list/ListContract.java
@@ -0,0 +1,11 @@
+package com.example.sandy.accountingapp.list;
+
+public interface ListContract {
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ app/src/main/java/com/example/sandy/accountingapp/util/Value.java
@@ -0,0 +1,5 @@
+package com.example.sandy.accountingapp.util;
+
+public class Value {
+    //专门用于 public static final int
+}
