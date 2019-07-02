798394ace3cb57f345b398c5ae14567c6f50b89b
sandy <crazysandy7342@gmail.com>
2019-07-02 09:18:59 +0800

    version7.2.3 zxx

---
+++ app/src/main/java/com/example/sandy/accountingapp/util/ActivityUtils.java
@@ -0,0 +1,12 @@
+package com.example.sandy.accountingapp.util;
+
+import java.util.List;
+
+public class ActivityUtils {
+    private ActivityUtils() {
+    }
+
+    private static ActivityUtils INSTANCE = new ActivityUtils();
+
+
+}
