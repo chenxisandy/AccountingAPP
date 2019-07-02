fece80822390ce267321a41049110451ad0e78b7
sandy <crazysandy7342@gmail.com>
2019-07-02 09:31:36 +0800

    deal with case problem

---
+++ app/src/main/java/com/example/sandy/accountingapp/list/AccountAdapter.java
@@ -0,0 +1,100 @@
+package com.example.sandy.accountingapp.list;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.sandy.accountingapp.R;
+import com.example.sandy.accountingapp.model.Account;
+
+import java.util.List;
+
+
+public class AccountAdapter extends RecyclerView.Adapter<AccountAdapter.ViewHolder>{
+
+    private List<Account> accountList;
+
+    private Context mContext;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        if (mContext == null) {
+            mContext = viewGroup.getContext();
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Account account = accountList.get(i);
+
+        if (!account.isSignal()) {  //如果金额为负
+            viewHolder.textMoney.setText("-" + account.getMoney() + "元");
+        } else {
+            viewHolder.textMoney.setText(account.getMoney() + "元");
+        }
+
+        viewHolder.textTime.setText(account.getYear() + "年" + account.getMonth() + "月" + account.getDay() + "日");
+
+        switch (account.getType()) {
+            case Account.CLOTH:
+                viewHolder.typeImg.setImageResource(R.drawable.clothe);
+                break;
+            case Account.EAT:
+                viewHolder.typeImg.setImageResource(R.drawable.eat);
+                break;
+            case Account.GO:
+                viewHolder.typeImg.setImageResource(R.drawable.car);
+                break;
+            case Account.STUDY:
+                viewHolder.typeImg.setImageResource(R.drawable.book);
+                break;
+            case Account.PLAY:
+                viewHolder.typeImg.setImageResource(R.drawable.play);
+                break;
+        }
+
+        viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 2019/7/1 add intent
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return accountList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+
+        CardView cardView;
+
+        TextView textMoney;
+
+        TextView textTime;
+
+        ImageView typeImg;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = itemView.findViewById(R.id.card_view);
+            textMoney = itemView.findViewById(R.id.money_text);
+            textTime = itemView.findViewById(R.id.time_text);
+            typeImg = itemView.findViewById(R.id.type_img);
+        }
+    }
+
+    public AccountAdapter(List<Account> accountList) {
+        this.accountList = accountList;
+    }
+}
---
+++ app/src/main/java/com/example/sandy/accountingapp/list/ListActivity.java
@@ -0,0 +1,89 @@
+package com.example.sandy.accountingapp.list;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.sandy.accountingapp.R;
+import com.example.sandy.accountingapp.model.Account;
+import com.example.sandy.accountingapp.model.LocalRepo;
+
+import java.util.List;
+
+public class ListActivity extends AppCompatActivity implements ListContract.View, NavigationView.OnNavigationItemSelectedListener {
+
+    private ListContract.Presenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_layout);
+        initView();
+    }
+
+    private void initView() {
+        presenter = new ListPresenter(this, LocalRepo.getInstance());
+        presenter.fillList();       //填补list已经包括了getIntent
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void setRecyclerView(List<Account> list) {
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        AccountAdapter adapter = new AccountAdapter(list);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public int getIndexFromIntent() {
+        Intent intent = getIntent();
+        return intent.getIntExtra(getString(R.string.user_index), 0);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // TODO: 2019/7/1 直接结束我们的app，并且储存好数据
+        finish();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.user_quit:
+                Toast.makeText(this, "退出登录成功", Toast.LENGTH_SHORT).show();
+                // TODO: 2019/7/1 处理数据，存好后回到登录界面
+                finish();
+                break;
+            case R.id.data_table:
+                Toast.makeText(this, "进入数据统计界面", Toast.LENGTH_SHORT).show();
+                // TODO: 2019/7/1 进入数据内容
+                break;
+            case R.id.setting:
+                Toast.makeText(this, "进入设置界面", Toast.LENGTH_SHORT).show();
+                //todo 进入设置
+                break;
+        }
+        return true;
+    }
+}
